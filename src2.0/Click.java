8c7e8e0a2017e24ac0eb1e1dc91bfdae7d29ee3b
Ashwath Reddy <rashwath55@gmail.com>
2020-02-15 18:28:57 -0700

    Added a Click class to draw a line between two points

---
+++ src2.0/Click.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import javax.swing.JLabel;
+ 
+public class Click
+{
+	private static int numPoints;
+	private int x1,x2,y1,y2;
+	
+	public void paintLine(Graphics g)
+	{
+		g.drawLine(x1, y1, x2, y2);
+	}
+	   
+	
+	
+	
+	
+	
+	
+}
