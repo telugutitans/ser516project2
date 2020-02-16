eafc58d53ae7f6456685146ee6f29108d01d6c00
Ashwath Reddy <rashwath55@gmail.com>
2020-02-15 19:42:07 -0700

    added constuctor to Click

--- src2.0/Click.java
+++ src2.0/Click.java
@@ -8,10 +8,23 @@ import javax.swing.JLabel;
  
 public class Click
 {
-	private static int numPoints;
-	private int x1,x2,y1,y2;
-	
-	public void paintLine(Graphics g)
+	private static int numPoints = 0;
+	private static int x1,x2,y1,y2;
+	public  Click(int x,int y)
+	{
+		if (numPoints == 0)
+		{
+			x1=x;
+			y1=y;
+		}
+		else
+		{
+			x2=x;
+			y2=y;
+			numPoints=0;
+		}
+	}
+	private void paintLine(Graphics g)
 	{
 		g.drawLine(x1, y1, x2, y2);
 	}
